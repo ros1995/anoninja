8456b530a11a0a43072b1dabb6aeb91b4c82b7a4
Roselyn Le <roselynle95@gmail.com>
2021-03-30 17:20:05 +0100

    create server

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
-const app = require('./server');
+const app = require("./server");
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`Express now departing from port ${port}!`))
+app.listen(port, () => console.log(`Express now departing from port ${port}!`));
