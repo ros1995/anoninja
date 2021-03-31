1db2c43a67f197ca10b4ffd64d0306c229d41aab
chris westerduin <chriswesterduin@outlook.com>
2021-03-31 09:21:43 +0100

    functions for rendering form / post

---
+++ client/static/js/layout.js
@@ -0,0 +1,37 @@
+window.addEventListener('hashchange', updateContent);
+window.addEventListener('load', updateContent);
+
+function updateContent() {
+    const id = window.location.hash.substring(1)
+    id.length > 0 ? renderPost(id) : renderForm()
+}
+
+function renderPost(postData){
+    const root = document.getElementById('root')
+    root.textContent = postData
+}
+
+const fields = [
+    { tag: 'input', attributes: {autocomplete : "off", required: true, type: 'text', name: 'title', placeholder: 'Title' } },
+    { tag: 'input', attributes: {autocomplete : "off", type: 'text', name: 'pseudonym', placeholder: 'Ninja name...' } },
+    { tag: 'textarea', attributes: { name: 'content', placeholder: 'Ninja post...' } },
+    { tag: 'input', attributes: { type: 'submit', value: 'submit' } }
+]
+
+function renderForm(){
+    const root = document.getElementById('root')
+    const form = document.createElement('form');
+        fields.forEach(f => {
+            const field = document.createElement(f.tag);
+            field.textContent = f.text;
+            Object.entries(f.attributes).forEach(([a, v]) => field.setAttribute(a, v))
+            form.appendChild(field);
+        })
+        form.onsubmit = submitPost;
+        root.appendChild(form);
+    }
+    
+function submitPost(e){
+    e.preventDefault()
+    console.log(JSON.stringify(Object.fromEntries(new FormData(e.target))))
+}
