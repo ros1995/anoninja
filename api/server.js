2b481f7bf48d4e23ad589b3ccce98fc713163e29
Roselyn Le <roselynle95@gmail.com>
2021-03-31 09:27:27 +0100

    update to link separate file for the routes

--- api/server.js
+++ api/server.js
@@ -5,7 +5,7 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
-const postsRoutes = require("./controllers/posts");
+const postsRoutes = require("./routes/postsRoutes");
 server.use("/posts", postsRoutes);
 
 // Root route
